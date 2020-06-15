6ce74c65dd33461e0f8a81eed5e9bdd736b9a1f4
Dianna Tingg <dtingg@gmail.com>
2020-06-14 20:17:35 -0700

    uncommented tests

--- src/data_structures/test/heap.test.js
+++ src/data_structures/test/heap.test.js
@@ -111,7 +111,7 @@ describe(MaxHeap, () => {
     });
   });
 
-  xdescribe('removeMax', () => {
+  describe('removeMax', () => {
     it('removes the only record, returns its element, and reduces the count', () => {
       heap.insert(3, 'test');
 
@@ -200,7 +200,7 @@ describe(MaxHeap, () => {
     return [null].concat(data.map(pri => ({ priority: pri, element: pri })));
   }
 
-  xdescribe("buildheap", () => {
+  describe("buildheap", () => {
     const buildAndVerify = (data) => {
       const heapInput = buildInputArray(data);
       const heap = new MaxHeap({ fromArray: heapInput });
@@ -232,7 +232,7 @@ describe(MaxHeap, () => {
     });
   });
 
-  xdescribe(MaxHeap.heapsort, () => {
+  describe(MaxHeap.heapsort, () => {
     const sortAndVerify = (data) => {
       const heapInput = buildInputArray(data);
       MaxHeap.heapsort(heapInput);
